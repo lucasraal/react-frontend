30331efe1844934874b0fd6fbe801d9fd7de3773
Dexfoot <lucasraal@gmail.com>
2022-01-02 06:18:38 -0300

    add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 
+import "./index.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Users from "./user/pages/Users";
+import Places from "./places/pages/Places";
+
 import { Outlet, Link } from "react-router-dom";
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/" element={<HomeElement />} />
+          <Route path="users" element={<Users />} />
+          <Route path="places" element={<Places />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
   return (
     <div>
       <h1>APP JS</h1>
@@ -11,8 +31,13 @@ function App() {
         <Link to="/places">Places</Link>
       </nav>
       <Outlet />
+      <h5>Add bottom component here</h5>
     </div>
   );
+}
+
+function HomeElement() {
+  return <h2>Home Element</h2>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Users from "./user/pages/Users";
-import Places from "./places/pages/Places";
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="users" element={<Users />} />
-        <Route path="places" element={<Places />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
